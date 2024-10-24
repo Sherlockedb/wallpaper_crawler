56dd4790b06944417a946504b4cec3d85863c8ce
Sherlockedb <1299670261@qq.com>
2024-10-24 18:37:09 +0800

    check dir

--- wallpaper_crawler/spiders/rare_gallery_setting.py
+++ wallpaper_crawler/spiders/rare_gallery_setting.py
@@ -1,2 +1,24 @@
-REQUEST_STORE = '/Users/sherlock/Project/wallpaper_crawler/.request/iu_iph_wallpaper_request.json'  # 图片保存的文件夹
+from pathlib import Path
+
+REQUEST_STORE = '/Users/sherlock/Project/wallpaper_crawler/.request/iu_iph_wallpaper_request.json'  # 请求信息保存路径
 IMAGES_STORE = '/Users/sherlock/Project/wallpaper_crawler/iu_iph_wallpaper'  # 图片保存的文件夹
+
+
+def _check_dir(directory):
+    if directory.exists():
+        return
+    directory.mkdir(parents=True)  # parents=True 允许创建多层目录
+
+def check_dir(dir_path):
+    directory = Path(dir_path)
+    return _check_dir(directory)
+
+
+def check_file_dir(file):
+    file_path = Path(file)
+    directory = file_path.parent  # 提取文件的父目录
+    return _check_dir(directory)
+
+
+check_file_dir(REQUEST_STORE)
+check_dir(IMAGES_STORE)
